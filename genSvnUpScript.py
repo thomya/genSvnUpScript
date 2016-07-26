5a90cb534ef61efd1f2544376a6b379c0c6ad273
Thomas.ZP <thomya@users.noreply.github.com>
2016-07-26 20:18:35 +0800

    upload new file
    commit genSvnUpScript.py firstly.

---
+++ genSvnUpScript.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python2.7
+
+import string
+
+print "This is little tools for getting app svn info"
+
+f=open("releasenote_V049.txt","r")
+f1=open("svnup.sh","w")
+key="http"
+i=0
+list1=[]
+
+for line in f.readlines():
+    if not line.strip():
+        continue
+    if(line.find(key) != -1):
+        i=0
+#        print "svn up -r" + list1[1]  + " " + list1[0]
+        f1.write("svn up -r " + list1[1]  + " " + list1[0] + "\n")
+        list1=[]
+    else:
+        list1.append(line.strip("\n"))
+        i=i+1
+f.close()
+f1.close()
+
+print "====== end ======"
