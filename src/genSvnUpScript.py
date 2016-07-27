c93c1d79c02ccea78f5b0673502146d42be18898
yangzp <yangzp@tcl.com>
2016-07-27 17:51:55 +0800

    add sourcepath & destpath

--- src/genSvnUpScript.py
+++ src/genSvnUpScript.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python2.7
 
 import string
+import sys
 
-print "This is little tools for getting app svn info"
-
-f=open("../releasenote_V049.txt","r")
-f1=open("svnup.sh","w")
+print "This is little tools for getting app svn info ;\n \
+Usage : python genSvnUpScript sourcepath destpath "
+if(len(sys.argv) < 3):
+    print " pls input sourcepath & destpath ,maybe you can refer to Usage : python genSvnUpScript sourcepath destpath "
+    exit(1)
+sourcepath=sys.argv[1]
+print sourcepath
+destpath=sys.argv[2]
+print destpath
+f=open(sourcepath,"r")
+f1=open(destpath,"w")
 key="http"
 i=0
 list1=[]
@@ -17,7 +25,7 @@ for line in f.readlines():
 #        print "svn up -r" + list1[1]  + " " + list1[0]
         list1.append(line.strip("\n"))
         f1.write("svn up -r " + list1[i-1]  + " " + list1[i-2] + "\n")
-        print list1
+#        print list1
 	i=0
         list1=[]
     else:
